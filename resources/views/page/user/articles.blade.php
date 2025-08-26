c7f9f4d852e9ab78096b980d7093ffdc58318c3a
adenrm <jangangantich84@gmail.com>
2025-08-26 10:38:14 +0700

    revisi

--- resources/views/page/user/articles.blade.php
+++ resources/views/page/user/articles.blade.php
@@ -33,9 +33,9 @@
                 <span class="text-gray-200">Baca sekarang <span class="font-medium">🕒 15 min read</span></span>
 
                                    <a href="#" class="bg-white/20 p-2 rounded-full backdrop-blur-sm hover:bg-white/30 transition-colors">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.684 13.342C8.882 13.065 9 12.729 9 12s-.118-.935-.316-1.258m0 2.516a3 3 0 010-2.516m0 2.516v2.516c0 .817.65 1.472 1.458 1.472h1.305C13.597 15 15 13.597 15 11.829V8.171c0-1.768-1.403-3.171-3.137-3.171h-1.305c-.808 0-1.458.655-1.458 1.472v2.516m0 0V9.342m0-2.516A3 3 0 019 6c0-.817.65-1.472 1.458-1.472h1.305c1.734 0 3.137 1.403 3.137 3.172v3.656m-6.507 1.638a3 3 0 01-1.458 1.472h-1.305C6.403 15 5 13.597 5 11.829v-3.657c0-1.768 1.403-3.172 3.137-3.172h1.305c.808 0 1.458.655 1.458 1.472v2.516m0 0V9.342m0-2.516a3 3 0 01-1.458 1.472h-1.305C6.403 15 5 13.597 5 11.829v-3.657c0-1.768 1.403-3.172 3.137-3.172h1.305c.808 0 1.458.655 1.458 1.472v2.516" />
-                        </svg>
+                            <svg class="w-full h-full text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"></path>
+                            </svg>
                     </a>
 
             </div>
