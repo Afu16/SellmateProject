5bfddf6d1defa49a6363d64fae81bc4185e817c3
adenrm <jangangantich84@gmail.com>
2025-09-16 20:52:34 -0700

    settings change

--- resources/views/page/user/setting.blade.php
+++ resources/views/page/user/setting.blade.php
@@ -38,83 +38,71 @@
         <h2 class="text-xl font-pilcrow font-pilcrow-bold text-black mb-4">Informasi Pribadi</h2>
         <div class="bg-primary border-2 border-black shadow-black rounded-lg p-6 mb-6">
             <!-- Name -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">Name</label>
-                        <input type="text" value="Siska Amelia" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                <label class="text-sm text-white font-pilcrow font-pilcrow-heavy ">Name</label>
+                    <div class="flex p-3 items-center bg-white rounded-lg h-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                        <input type="text" value="Siska Amelia" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- Email -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">Email</label>
-                        <input type="email" value="Siskaamella@gmail.com" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                <label class="text-sm text-white font-pilcrow font-pilcrow-heavy ">Email</label>
+                <div class="flex items-center p-3 bg-white rounded-lg h-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        </svg>
+                        <input type="email" value="Siskaamella@gmail.com" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- Phone -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">Phone</label>
-                        <input type="tel" value="082484333323" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                <label class="text-xs text-white font-pilcrow font-pilcrow-heavy ">Phone</label>
+                <div class="flex items-center p-3 bg-white rounded-lg h-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                        </svg>
+                        <input type="tel" value="082484333323" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- Location -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">Location</label>
-                        <input type="text" value="Bandung, Indonesia" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                <label class="text-sm text-white font-pilcrow font-pilcrow-heavy ">Location</label>
+                    <div class="flex bg-white rounded-lg h-10 items-center p-3"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <input type="text" value="Bandung, Indonesia" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- School -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">School</label>
-                        <input type="text" value="SMK BPI Bandung" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                <label class="text-sm text-white font-pilcrow font-pilcrow-heavy ">School</label>
+                    <div class="flex bg-white rounded-lg h-10 items-center p-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        </svg>
+                        <input type="text" value="SMK BPI Bandung" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- Department -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div class="mb-2 py-2">
+                <label class="text-sm text-white font-pilcrow font-pilcrow-heavy ">Department</label>
+                    <div class="flex bg-white rounded-lg h-10 items-center p-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                     </svg>
-                    <div class="flex-1">
-                        <label class="text-xs text-purple-600 font-pilcrow font-pilcrow-medium">Department</label>
-                        <input type="text" value="RPL" class="w-full text-black font-medium focus:outline-none" readonly>
-                    </div>
+                        <input type="text" value="RPL" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                 </div>
             </div>
         </div>
@@ -125,29 +113,23 @@
         <h2 class="text-xl font-pilcrow font-pilcrow-bold text-black mb-4">Keamanan Akun</h2>
         <div class="bg-primary shadow-black border-2 border-black rounded-lg p-6 mb-6">
             <!-- Username -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs font-pilcrow font-pilcrow-medium">Username</label>
-                        <input type="text" value="Siska Amelia02" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                    <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        <input type="text" value="Siska Amelia02" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
 
             <!-- Password -->
-            <div class="mb-4 py-2">
-                <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                    </svg>
-                    <div class="flex-1">
-                        <label class="text-xs font-pilcrow font-pilcrow-medium">Password</label>
-                        <input type="password" value="********" class="w-full text-black font-medium focus:outline-none" readonly>
+            <div class="mb-2 py-2">
+                    <div class="flex items-center bg-white border-2 border-black shadow-black rounded-lg p-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                        </svg>
+                        <input type="password" value="********" class="w-full text-sm font-quicksand font-quicksand-regular focus:border-none focus:outline-none focus:ring-0 border-0 bg-transparent" readonly>
                     </div>
-                </div>
             </div>
         </div>
     </div>
